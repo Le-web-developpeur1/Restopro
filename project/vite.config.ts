54b14e87179ffbd5e2b09422b2524dd5045d54d1
Lemagiciendelombre <booba8033@gmail.com>
2026-04-09 17:23:42 +0000

    Ajout du PWA

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'RestoPro POS',
+        short_name: 'Restopro',
+        description: 'POS Gestion Restaurant',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/images/RestoPro.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/images/RestoPro.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
